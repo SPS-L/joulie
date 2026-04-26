4c8701116e975cee72c1410fcfaa99ef3e8e8e51
Petros Aristidou <apetros@pm.me>
2026-04-26 21:02:11 +0300

    build(data): enable Room schema export to app/schemas/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,11 @@ android {
     }
 }
 
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+    arg("room.incremental", "true")
+}
+
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
